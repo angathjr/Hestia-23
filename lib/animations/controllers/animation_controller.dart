54b21281c458fdc11e2c670cd287199da48ef608
angathjr <angathjr@gmail.com>
2023-04-22 03:12:31 +0530

    added animations for schedule

--- lib/animations/controllers/animation_controller.dart
+++ lib/animations/controllers/animation_controller.dart
@@ -5,7 +5,7 @@ class AnimController extends GetxController {
   var start = true.obs;
 
   void loadScheduleAnimation() async {
-    await Future.delayed(const Duration(milliseconds: 600), () {
+    await Future.delayed(const Duration(milliseconds: 1000), () {
       start.value = false;
     });
   }
