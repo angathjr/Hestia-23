288be2058be8246e6528dc7f12d83c106ac6af42
hrshankar2002 <hrshankar2002@gmail.com>
2023-04-22 06:10:01 +0530

    revised leaderboard ui 2

--- lib/home/views/leaderboard_card.dart
+++ lib/home/views/leaderboard_card.dart
@@ -9,124 +9,6 @@ import '../../core/Constants..dart';
 class LeaderBoard extends StatelessWidget {
   const LeaderBoard({super.key});
 
-  final style1 = const TextStyle(fontWeight: FontWeight.bold, fontSize: 24);
-  final style2 = const TextStyle(
-    fontSize: 20,
-    fontWeight: FontWeight.bold,
-  );
-
-  // leaderboard widget-past theme
-  Widget pastLeaderboard(num h, BuildContext context) {
-    return Column(
-      children: [
-        Center(child: Image.asset('assets/images/past_lb.png')),
-        Padding(
-          padding: const EdgeInsets.all(15),
-          child: TextField(
-            controller: null,
-            decoration: InputDecoration(
-              contentPadding: const EdgeInsets.symmetric(vertical: 5.0),
-              prefixIcon: const Icon(
-                Icons.search,
-                color: Colors.white,
-              ),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              hintStyle: const TextStyle(color: Colors.white),
-              hintText: " Search for event",
-            ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: Container(
-            height: MediaQuery.of(context).orientation == Orientation.portrait
-                ? h * 0.4
-                : h * 0.35,
-            decoration: const BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(8)),
-                color: Color.fromARGB(255, 23, 22, 22)),
-            child: ListView.builder(
-              itemCount: 10,
-              itemBuilder: (BuildContext context, int index) {
-                return const ListTile(
-                    trailing: Icon(
-                      Icons.arrow_forward,
-                      color: Colors.red,
-                    ),
-                    title: Padding(
-                      padding: EdgeInsets.only(left: 10),
-                      child: Text(
-                        "Logo Designing",
-                        style: TextStyle(color: Colors.white
-                            //fontWeight: FontWeight.bold,
-                            ),
-                      ),
-                    ));
-              },
-            ),
-          ),
-        )
-      ],
-    );
-  }
-
-  // leaderboard widget-present theme
-  Widget presentLeaderboard(num h, BuildContext context) {
-    return Column(
-      children: [
-        Center(child: Image.asset('assets/images/present_lb.png')),
-        Padding(
-          padding: const EdgeInsets.all(15),
-          child: TextField(
-            controller: null,
-            decoration: InputDecoration(
-              contentPadding: const EdgeInsets.symmetric(vertical: 5.0),
-              prefixIcon: const Icon(
-                Icons.search,
-                color: Colors.white,
-              ),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              hintStyle: const TextStyle(color: Colors.white),
-              hintText: " Search for event",
-            ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: Container(
-            height: MediaQuery.of(context).orientation == Orientation.portrait
-                ? h * 0.4
-                : h * 0.35,
-            decoration: const BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(8)),
-                color: Color.fromARGB(255, 23, 22, 22)),
-            child: ListView.builder(
-              itemCount: 10,
-              itemBuilder: (BuildContext context, int index) {
-                return const ListTile(
-                    trailing: Icon(
-                      Icons.arrow_forward,
-                      color: Colors.yellow,
-                    ),
-                    title: Padding(
-                      padding: EdgeInsets.only(left: 10),
-                      child: Text(
-                        "Logo Designing",
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ));
-              },
-            ),
-          ),
-        )
-      ],
-    );
-  }
-
   //leaderboard widget-future theme
   static Widget futureLeaderboard(num h, BuildContext context) {
     return Padding(
@@ -180,18 +62,22 @@ class LeaderBoard extends StatelessWidget {
                         height: h * 0.4,
                         child: Stack(
                           fit: StackFit.expand,
-                          children: const [
+                          children: [
                             Align(
-                              alignment: Alignment(-1, 0),
-                              child: StackItem(),
+                              alignment: const Alignment(-1, 0),
+                              child: StackItem(
+                                number: 2,
+                              ),
                             ),
                             Align(
-                              alignment: Alignment(0, -0.4),
+                              alignment: const Alignment(0, -0.4),
                               child: StackItem(large: true),
                             ),
                             Align(
-                              alignment: Alignment(1, 0),
-                              child: StackItem(),
+                              alignment: const Alignment(1, 0),
+                              child: StackItem(
+                                number: 3,
+                              ),
                             )
                           ],
                         ),
@@ -212,7 +98,6 @@ class LeaderBoard extends StatelessWidget {
     final height = MediaQuery.of(context).size.height -
         MediaQuery.of(context).padding.top -
         kToolbarHeight;
-    final width = MediaQuery.of(context).size.width;
 
     return SafeArea(
       child: Scaffold(
@@ -233,8 +118,9 @@ class LeaderBoard extends StatelessWidget {
 }
 
 class StackItem extends StatelessWidget {
-  const StackItem({super.key, this.large = false});
+  StackItem({super.key, this.large = false, this.number = 1});
   final bool large;
+  final num number;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -268,12 +154,12 @@ class StackItem extends StatelessWidget {
                   width: 26.0,
                   height: 30.0,
                   decoration: BoxDecoration(
-                      color: Colors.deepPurple,
+                      color: const Color.fromARGB(255, 58, 23, 118),
                       borderRadius: BorderRadius.circular(4.0)),
                   child: Padding(
                     padding: const EdgeInsets.all(8.0),
-                    child:
-                        Center(child: Text('2', style: FutTheme.categoryFont)),
+                    child: Center(
+                        child: Text('${number}', style: FutTheme.categoryFont)),
                   ),
                 ),
               ),
