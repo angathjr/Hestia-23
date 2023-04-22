e895becb9b7649e00cd58c0da164712ed6a94dbd
Neeraj S D <neerajsd04@gmail.com>
2023-04-22 12:16:38 +0530

    notification integration for general and my events

--- lib/notifications/models/notification.dart
+++ lib/notifications/models/notification.dart
@@ -13,9 +13,17 @@ String notificationModelToJson(List<NotificationModel> data) =>
     json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
 
 class NotificationModel {
-  NotificationModel({this.message, this.email, this.username, this.createdAt});
+  NotificationModel(
+      {this.message,
+      this.email,
+      this.username,
+      this.createdAt,
+      this.title,
+      this.eventName});
 
   String? message;
+  String? title;
+  String? eventName;
   String? email;
   String? username;
   DateTime? createdAt;
@@ -25,12 +33,16 @@ class NotificationModel {
           message: json["message"],
           email: json["email"],
           username: json["username"],
-          createdAt: (json["createdAt"] as Timestamp).toDate());
+          createdAt: (json["createdAt"] as Timestamp).toDate(),
+          title: json["title"],
+          eventName: json["eventName"]);
 
   Map<String, dynamic> toJson() => {
         "message": message,
         "email": email,
         "username": username,
         "createdAt": createdAt,
+        "title": title,
+        "eventName": eventName,
       };
 }
