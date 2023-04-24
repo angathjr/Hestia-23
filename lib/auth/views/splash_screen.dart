9e8abfe187ee5c8c4cf7fbc0e324635b7be0eb20
angathjr <angathjr@gmail.com>
2023-04-24 21:47:21 +0530

    added splash screen

---
+++ lib/auth/views/splash_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  void loadPages() async {
+    await Future.delayed(const Duration(milliseconds: 500));
+    Get.offAllNamed('/');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    loadPages();
+    return SizedBox(
+        width: double.infinity,
+        height: double.infinity,
+        child: Image.asset("assets/images/loginfinal.png"));
+  }
+}
